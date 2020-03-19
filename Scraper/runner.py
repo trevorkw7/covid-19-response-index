6d9a3f676954bf5acb07475ed126f8d788bffe21
trevorkw7 <54345106+trevorkw7@users.noreply.github.com>
2020-03-19 13:57:37 -0700

    Created runner with 60 second time intervals

--- Scraper/runner.py
+++ Scraper/runner.py
@@ -1,12 +1,16 @@
 import time
+from timeloop import Timeloop
+from datetime import timedelta
 import parsedata
 import testspercountrywebscraper
 
+t1 = Timeloop()
+
+@t1.job(interval=timedelta(seconds=1))
 def run():
+    print("execute")
     testspercountrywebscraper.scrape()
     parsedata.parse()
 
-while True:
-    print("execute")
-    run()
-    time.sleep(10)
+if __name__ == "__main__":
+    t1.start(block=True)
